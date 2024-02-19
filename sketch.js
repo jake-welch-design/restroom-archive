fc4aa4e70a5f3c8c0ca3a9e5309c014ddf9027d5
Jake Welch <94939899+jake-welch-design@users.noreply.github.com>
2024-02-18 17:53:04 -0700

    Update sketch.js
    More WEBGL texture troubleshooting for mobile.

--- sketch.js
+++ sketch.js
@@ -63,6 +63,7 @@ function loadTextureAndApplyParameters(path) {
     gl.bindTexture(gl.TEXTURE_2D, img._renderer.glTexture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   });
 }
 
